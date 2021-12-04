8f737161481ba8e85974b8c77f56ab0da837708c
Felipe Martins <fmartins809@gmail.com>
2021-12-04 13:02:48 -0300

    Project created

---
+++ src/applicatiion/Program.java
@@ -0,0 +1,10 @@
+package applicatiion;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("hello word");
+	}
+
+}
